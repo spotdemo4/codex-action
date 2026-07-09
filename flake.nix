a7c95ae58a8735d95a1f2f66b7b8618eddee72f8
spotdemo4 <me@trev.xyz>
2026-07-09 11:39:55 -0400

    bump: v0.1.0 -> v0.1.1

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
           default = pkgs.buildNpmPackage (
             final: with pkgs.lib; {
               pname = "codex-action";
-              version = "0.1.0";
+              version = "0.1.1";
 
               src = fileset.toSource {
                 root = ./.;
