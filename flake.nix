fd133063dc6ebe79decdd4f62058321c7c9d1bb9
doctor <doctor@trev.xyz>
2026-07-16 22:02:30 +0000

    bump: v0.3.1 -> v0.3.2

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
           default = pkgs.buildNpmPackage (
             final: with pkgs.lib; {
               pname = "codex-action";
-              version = "0.3.1";
+              version = "0.3.2";
 
               src = fileset.toSource {
                 root = ./.;
